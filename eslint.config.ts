cf180123d0829510a0396e8c4db4d381fc6102dd
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-08-10 22:01:26 +0100

    chore: remove prettier eslint plugin
    It gave warnings that were automatically fixed with prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,9 @@
-import js from "@eslint/js";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginAstro from "eslint-plugin-astro";
-import pluginPrettier from "eslint-plugin-prettier/recommended";
-import { defineConfig } from "eslint/config";
 
 export default tseslint.config(
   // pluginReact.configs.flat.recommended,
   ...tseslint.configs.recommended,
-  pluginPrettier,
   pluginAstro.configs.recommended,
 );
