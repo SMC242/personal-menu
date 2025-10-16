2ef82356f5ef928e2a3e774704e15de135104328
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-10-16 01:33:13 +0100

    refactor: extract dish name record
    Extracts computing the dish names and components to its own file and
    computes it only once. This will make implementing an index of all
    dishes easier

---
+++ src/utils/allDishes.ts
@@ -0,0 +1,48 @@
+import { readdir } from "node:fs/promises";
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+
+export type DishMetadata = {
+  slug: string;
+  title: string;
+  component: AstroComponentFactory;
+};
+
+// From https://stackoverflow.com/a/67243723
+function toKebabCase(camelCased: string): string {
+  return camelCased.replace(
+    /[A-Z]+(?![a-z])|[A-Z]/g,
+    ($, ofs) => (ofs ? "-" : "") + $.toLowerCase(),
+  );
+}
+
+// From https://stackoverflow.com/a/7225450
+function toTitleCase(camelCased: string): string {
+  const result = camelCased.replace(/([A-Z])/g, " $1");
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+async function getAllDishes(): Promise<Record<string, DishMetadata>> {
+  // Weird things going on with the path here
+  const dishNames = await readdir("./src/dishes");
+  console.debug(dishNames);
+  const components = await Promise.all(
+    dishNames.map((name) =>
+      import(`../dishes/${name}`).then((module) => module.default),
+    ),
+  );
+
+  return dishNames.reduce((obj, name, idx) => {
+    const slug = toKebabCase(name.replace(".astro", ""));
+
+    return {
+      ...obj,
+      [slug]: {
+        slug: slug,
+        title: toTitleCase(name),
+        component: components[idx],
+      },
+    };
+  }, {});
+}
+
+export const allDishes = await getAllDishes();
