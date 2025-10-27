cccf9cfca64b760478fb983a95d553e5d1ac92a0
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-10-27 23:07:45 +0000

    feat: bookmark component
    A little red skeuomorphic bookmark

---
+++ src/components/bookmark.tsx
@@ -0,0 +1,29 @@
+import "@styles/bookmark.css";
+import { type PropsWithChildren, type HTMLAttributes } from "react";
+
+export type BookmarkProps = PropsWithChildren<{
+  variant: "light" | "dark";
+}> &
+  HTMLAttributes<HTMLDivElement>;
+
+function classFromVariant(variant: BookmarkProps["variant"]) {
+  switch (variant) {
+    case "light":
+      return "bookmark bookmark--light";
+    case "dark":
+      return "bookmark";
+  }
+}
+export default function Bookmark({
+  className,
+  variant = "dark",
+  children,
+}: BookmarkProps) {
+  return (
+    <div
+      className={`${classFromVariant(variant)} bookmark-gradient min-w-max ${className ?? ""}`}
+    >
+      {children}
+    </div>
+  );
+}
