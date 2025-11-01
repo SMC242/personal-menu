086c46708f260b76620d7b67357f2dd3e85033b4
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-11-01 03:25:37 +0000

    feat!: add DishType to RecipeTags type
    Adds an extra tag for whether the dish is a main, side, breakfast, etc

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,7 +2,16 @@ export type Spiciness = "Not spicy" | "Mild" | "Medium" | "Hot";
 
 export type Cuisine = "North Indian";
 
+export type DishType =
+  | "Main"
+  | "Side"
+  | "Carbs"
+  | "Drink"
+  | "Dessert"
+  | "Breakfast";
+
 export type RecipeTags = {
   cuisine: Cuisine;
   spiciness: Spiciness;
+  dishType: DishType;
 };
